c49bf76061f09e47dcdbd62cd30babfeb2110fdc
Yan Vugenfirer <yan@daynix.com>
2020-08-23 16:54:47 +0300

    Add basic controllers (paths) to the web server
    Add:
    /
    /qemu
    /setups
    /storage
    
    Currently those are stubs for the future information that will be displayed.
    
    Signed-off-by: Yan Vugenfirer <yan@daynix.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+#![feature(decl_macro)]
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello world!"
+}
+
+#[get("/qemu")]
+fn qemu() -> &'static str {
+    "Hello world!"
+}
+
+#[get("/setups")]
+fn setups() -> &'static str {
+    "Hello world!"
+}
+
+#[get("/storage")]
+fn storage() -> &'static str {
+    "Hello world!"
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![index, qemu, setups, storage]).launch();
 }
