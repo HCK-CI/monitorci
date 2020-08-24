6ad52e33fc73e4fbe41f7e0c2456b06ea8f13b73
Yan Vugenfirer <yan@daynix.com>
2020-08-24 18:35:29 +0300

    Add infrastructure to use templates for HTML rendering
    Signed-off-by: Yan Vugenfirer <yan@daynix.com>

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 #![feature(decl_macro)]
 #[macro_use] extern crate rocket;
+extern crate rocket_contrib;
+extern crate tera;
 
+use rocket_contrib::templates::Template;
+use rocket_contrib::serve::StaticFiles;
+use tera::Context;
 mod system_info;
 
 #[get("/")]
-fn index() -> String {
-    system_info::get_system_info()
+fn index() -> Template {
+    let mut context = Context::new();
+
+    context.insert("host_name", &system_info::get_system_info());
+    // Where `base` is the name of the template
+    Template::render("base", &context.into_json())
 }
 
 #[get("/qemu")]
@@ -24,5 +33,10 @@ fn storage() -> String {
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index, qemu, setups, storage]).launch();
+    rocket::ignite()
+        .mount("/vendor", StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/www/vendor")))
+        .mount("/css", StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/www/css")))
+        .mount("/", routes![index, qemu, setups, storage])
+        .attach(Template::fairing())
+        .launch();
 }
